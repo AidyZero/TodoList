292b4d3c8fc42d1c453e600cc1578f2c093fd4bf
yamble chris <yamble999@gmail.com>
2023-02-02 02:18:54 +0100

    Ajout dun fichier javascript pour rendre l'application dynamique

---
+++ src/script/script.js
@@ -0,0 +1,22 @@
+
+const task_field = document.querySelector('#task_field')
+
+const btn_add = document.querySelector('#btn_add') 
+
+const tasks_area = document.querySelector('#tasks_area')
+
+const task_row = document.querySelectorAll('.task_row')
+
+const task_content = document.querySelectorAll('.task_content')
+
+const btn_update = document.querySelectorAll('.btn_update')
+
+const btn_delete = document.querySelectorAll('.btn_delete')
+
+
+alert(btn_update)
+console.log(btn_update)
+
+
+
+
